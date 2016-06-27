ea7ddd0d4c2b55004bd716099573c8bef9d13adb
hyounwoo.ko <lj1212.kit@gmail.com>
2016-06-27 14:28:36 +0900

    jasmine 테스트 환경 구축

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+'use strict';
+
 let gulp = require('gulp'),
   babel = require('gulp-babel'),
-  print = require('gulp-print'),
+  gPrint = require('gulp-print'),
   stripCode = require('gulp-strip-code'),
   stripComments = require('gulp-strip-comments'),
   webserver = require('gulp-webserver'),
+  jasmine = require('gulp-jasmine'),
   del = require('del');
 
 const DIST_PATH = 'dist';
@@ -12,7 +15,7 @@ const DEPLOY_PATH = 'deploy';
 /** 개발용 */
 gulp.task('js:dist', function () {
   return gulp.src('app/**/*.js')
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(babel())
     .pipe(gulp.dest(DIST_PATH));
 });
@@ -23,7 +26,7 @@ gulp.task('libs:dist', function () {
       'node_modules/systemjs/dist/system.js',
       'node_modules/babel-polyfill/dist/polyfill.js'
     ])
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(gulp.dest(DIST_PATH + '/libs'));
 });
 
@@ -35,7 +38,7 @@ gulp.task('clean:dist', function () {
 /** 개발용 */
 gulp.task('build', ['clean:dist', 'js:dist', 'libs:dist'], function () {
   return gulp.src(['app/**/*.html', 'app/**/*.css'])
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(gulp.dest(DIST_PATH));
 });
 
@@ -45,6 +48,12 @@ gulp.task('serve', ['build'], function () {
     .pipe(webserver({
       open: true
     }));
+});
+
+/** 테스트 */
+gulp.task('tdd', ['build'], () => {
+  gulp.src('dist/**/*.spec.js')
+    .pipe(jasmine());
 });
 
 /** 개발용 */
@@ -57,7 +66,7 @@ gulp.task('js:deploy', function () {
   return gulp.src('app/**/*.js')
     .pipe(stripCode())
     .pipe(stripComments())
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(babel())
     .pipe(gulp.dest(DEPLOY_PATH));
 });
@@ -70,7 +79,7 @@ gulp.task('libs:deploy', function () {
     ])
     .pipe(stripCode())
     .pipe(stripComments())
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(gulp.dest(DEPLOY_PATH + '/libs'));
 });
 
@@ -82,7 +91,7 @@ gulp.task('clean:deploy', function () {
 /** 배포용 */
 gulp.task('deploy', ['clean:deploy', 'js:deploy', 'libs:deploy'], function () {
   return gulp.src(['app/**/*.html', 'app/**/*.css'])
-    .pipe(print())
+    .pipe(gPrint())
     .pipe(gulp.dest(DEPLOY_PATH));
 });
 
