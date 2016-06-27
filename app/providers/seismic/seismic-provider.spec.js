12a6f8d0c4d4859e617378c4e48c50d4822bddca
hyounwoo.ko <lj1212.kit@gmail.com>
2016-06-27 15:47:37 +0900

    seismic-provider 테스트 모듈 구현

---
+++ app/providers/seismic/seismic-provider.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import {
+  SeismicProvider
+} from './seismic-provider';
+import {
+  SeismicOption
+} from '../../models/seismic-option';
+
+let option;
+let seismic;
+let seismicList;
+
+describe('지진조회 처리 모듈', function () {
+  beforeAll(function (done) {
+    option = new SeismicOption();
+    option.StartTm = '2016-04-01';
+    option.EndTm = '2016-04-31';
+
+    seismic = new SeismicProvider();
+    seismic.find(option)
+      .then(r => {
+        seismicList = r;
+        done();
+      }, e => {
+        console.log(`seismic.find() error`);
+        console.log(e);
+        done();
+      });
+  });
+
+  it('seismicList is valid', function () {
+    expect(seismicList).not.toBeUndefined();
+    expect(seismicList).not.toBeNull();
+  });
+
+  it('seismicList has list', function () {
+    expect(seismicList.length).toBeGreaterThan(0);
+    expect(seismicList.length).toBeLessThan(100);
+  });
+});
