a989f57373915fd34c5376bc2d4a77f84839481f
hyounwoo.ko <lj1212.kit@gmail.com>
2016-06-29 08:00:19 +0900

    기능 추가

--- app/providers/util.js
+++ app/providers/util.js
@@ -1,9 +1,12 @@
 'use strict';
+
+const fs = require('fs');
+
 /**
  * 유틸리티 라이브러리 클래스
  * @class
  */
-export class Util {
+class Util {
   /**
    * Promise then 사용을 위한 단순 출력 객체
    * @param {Object} item 출력할 객체
@@ -18,4 +21,21 @@ export class Util {
       resolve(item);
     });
   }
+
+  /**
+   * 동기식으로 파일을 읽어서 JSON으로 반환
+   * @param {string} file 파일 경로
+   * @return {array} JSON 배열
+   */
+  toJsonSync(file) {
+    try {
+      let reads = fs.readFileSync(file, 'utf8');
+      return JSON.parse(reads);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
 }
+
+export const util = new Util();
