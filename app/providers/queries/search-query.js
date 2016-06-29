9909a035bbedda6bfa8f0efcd4be49253bdd24c2
hyounwoo.ko <lj1212.kit@gmail.com>
2016-06-29 15:11:50 +0900

    검색 쿼리 클래스 시간 범위 객체 추가

--- app/providers/queries/search-query.js
+++ app/providers/queries/search-query.js
@@ -1,4 +1,5 @@
 'use strict';
+require('source-map-support').install();
 
 /**
  * 조회 쿼리
@@ -11,11 +12,34 @@ export class SearchQuery {
     return 'SearchQuery class';
   }
 
-  /** @member {string} stime */
-  set Stime(stime) {
-    this.stime = stime;
+  /** @member {string} stime 시작시간 */
+  set stime(stime) {
+    this._stime = stime;
   }
-  get Stime() {
-    return this.stime;
+  get stime() {
+    return this._stime;
+  }
+
+  /** @member {string} etime 종료시간 */
+  set etime(etime) {
+    this._etime = etime;
+  }
+  get etime() {
+    return this._etime;
+  }
+
+  /**
+   * Object로 반환
+   * @return {Object} 변환된 객체
+   */
+  toObject() {
+    let obj = {};
+    if (this.stime) {
+      obj.stime = this.stime;
+    }
+    if (this.etime) {
+      obj.etime = this.etime;
+    }
+    return obj;
   }
 }
